d88e5c6a8673ffc6ea9c90cb31f4f0e66151beb3
Rose Maina <rosewairimu27@gmail.com>
2022-07-21 09:24:23 +0300

    Done.

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { actors } from "../data";
 
+ 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  const actorz = actors.map((actor) =>(
+    <div key={actor.name}>
+      <h1>Name: {actor.name}</h1>
+      <ul>
+        Movies:{actor.movies.map((mov) =>(
+          <li key={mov}>
+            {mov}
+          </li>
+      ))}</ul>
+      <div>
+      </div>
+    </div>
+  ))
+  return (
+    <div>
+      <div>
+      <h1>Actors Page</h1>
+      </div>{actorz}<div>
+
+    </div>
+  </div>
+  )
 }
 
 export default Actors;
--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,8 +1,30 @@
 import React from "react";
 import { directors } from "../data";
 
+ 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorz = directors.map((director) =>(
+    <div key={director.name}>
+      <h2>Name: {director.name}</h2>
+        <ul>
+        Movies:{director.movies.map((mov,index) =>(
+          <li key={mov}>
+            {mov}
+          </li>
+      ))}</ul>
+      <div>
+      </div>
+    </div>
+  ))
+  return (
+    <div>
+      <div>
+      <h1>Directors Page</h1>
+      </div>{directorz}<div>
+
+    </div>
+  </div>
+  )
 }
 
 export default Directors;
